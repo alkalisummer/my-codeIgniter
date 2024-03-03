1448fdd9b6e373489cc385c74a6bf4be8c73049f
khkang <alkalisummer@gmail.com>
2024-03-03 10:47:52 +0900

    topic controller practice

--- application/controllers/topic.php
+++ application/controllers/topic.php
@@ -1,9 +1,30 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
 class Topic extends CI_Controller {
-
-	public function index()
-	{
-		echo '토픽 페이지';
-	}
+    function index(){
+        echo '
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8"/>
+            </head>
+            <body>
+                토픽 메인 페이지
+            </body>
+        </html>
+        ';
+    }
+    function get($id){
+        echo '
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8"/>
+            </head>
+            <body>
+                토픽 '.$id.'
+            </body>
+        </html>
+        ';
+    }
 }
+?>
