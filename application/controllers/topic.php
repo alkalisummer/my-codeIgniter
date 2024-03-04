5ea533bad7fa6c5e9431898b4d0e1fb43d889124
khkang <alkalisummer@gmail.com>
2024-03-05 01:36:10 +0900

    mySql 연동

--- application/controllers/topic.php
+++ application/controllers/topic.php
@@ -1,14 +1,17 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Topic extends CI_Controller {
     function index(){
+      $this->load->database();
+      $this->load->model('topic_model');
+      $data = $this->topic_model->gets();
       $this->load->view('head');    
-      $this->load->view('topic');    
+      $this->load->view('main', array('topics'=>$data));    
       $this->load->view('footer');    
     }
     function get($id){
       $data = array('id'=>$id);
       $this->load->view('head');
-      $this->load->view('main', $data);
+      $this->load->view('get', $data);
       $this->load->view('footer');    
     }
 }
